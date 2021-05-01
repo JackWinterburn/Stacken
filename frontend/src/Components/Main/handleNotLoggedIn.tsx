a6a7745223a230d90c6d2ecc73a60fbf50e97e6d
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2021-05-01 18:30:55 +0100

    fixed issue with user being sent to main page even if not being logged in

--- frontend/src/Components/Main/handleNotLoggedIn.tsx
+++ frontend/src/Components/Main/handleNotLoggedIn.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import { useSelector, RootStateOrAny } from "react-redux"
 import { Redirect } from "react-router-dom"
 
-export function HandleNotLoggedIn() {
-    const isLoggedIn = useSelector((state: RootStateOrAny) => state.isLoggedIn)
-    const cookie = document.cookie
+function getCookie(name: string): string | null {
+	const nameLenPlus = (name.length + 1);
+	return document.cookie
+		.split(';')
+		.map(c => c.trim())
+		.filter(cookie => {
+			return cookie.substring(0, nameLenPlus) === `${name}=`;
+		})
+		.map(cookie => {
+			return decodeURIComponent(cookie.substring(nameLenPlus));
+		})[0] || null;
+}
 
-    if (!isLoggedIn || cookie === "") {
+export function HandleNotLoggedIn() {
+    console.log(getCookie("UUID"))
+    const isLoggedIn = useSelector((state: RootStateOrAny) => state.isLoggedIn)
+    const UUID = getCookie("UUID")
+
+    if (!isLoggedIn || UUID === null) {
         return (
             <Redirect to="login" />
         )
